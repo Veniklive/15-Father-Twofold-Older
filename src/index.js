20afe3726074a16ab3949428937965f0c5affdd9
Veniklive <danilvivivi@gmail.com>
2025-01-29 00:10:09 +0200

    Fix function logic, Add extended output

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
 /** Calculates in how many years, or how many years ago, the father will be or was twice as old as the son.
  * @param {number} fatherYears - The number of years of the father.
  * @param {number} sonYears - The number of years of the son.
- * @returns {(number|String)} The calculated value or a notification about incorrectly entered data.
+ * @returns {String} The string with calculated value or a notification about incorrectly entered data.
  */
-function yearsDifferenceCalc(fatherYears, sonYears) {
+function calculateYearsDifference(fatherYears, sonYears) {
   if (
-    Math.abs(fatherYears - sonYears) >= 15 &&
+    fatherYears > sonYears &&
+    fatherYears - sonYears >= 15 &&
     0 < fatherYears &&
     0 < sonYears
   ) {
-    return Math.abs(fatherYears - sonYears * 2);
-  } else {
-    return "Invalid input detected";
+    switch (true) {
+      case fatherYears / sonYears === 2: {
+        return "The father is currently twice as old as the son.";
+      }
+      case fatherYears / sonYears > 2: {
+        return `The father was twice as old as the son ${Math.abs(
+          fatherYears - sonYears * 2
+        )} years ago.`;
+      }
+      case fatherYears / sonYears < 2: {
+        return `The father will be twice as old as the son in ${Math.abs(
+          fatherYears - sonYears * 2
+        )} years.`;
+      }
+      default: {
+        return "Invalid input detected";
+      }
+    }
   }
+  return "Invalid input detected";
 }
+
 let father = Number(prompt("Input father years: "));
 let son = Number(prompt("Input son years: "));
 
-console.log(yearsDifferenceCalc(father, son));
+console.log(calculateYearsDifference(father, son));
